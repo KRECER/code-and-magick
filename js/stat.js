5647be7656d1b026c2beb14bc804d39835cf273d
Igor Sudenko <sudenkoweb@gmail.com>
2019-03-08 07:09:01 +0200

    .setup: added similar-wizards;

--- js/stat.js
+++ js/stat.js
@@ -32,7 +32,7 @@ var printNames = function(ctx, playerName, x, y) {
   ctx.fillText(playerName, x, y);
 };
 
-var printScore = function() {
+var printScore = function(ctx, time) {
     
 };
 
@@ -40,19 +40,27 @@ var getHeightGist = function(score, maxScore) {
   return (GIST_MAX_HEIGHT * Math.floor(score)) / maxScore;
 };
 
-var showWinners = function(ctx, players, time) {
+var printWinners = function(ctx, players, time) {
   var GIST_X = CLOUD_X + 40;
   var GIST_Y = CLOUD_Y + CLOUD_HEIGHT - 40;
-  var GIST_POS = CLOUD_Y + CLOUD_HEIGHT - 30;
+  var NAME_POS = CLOUD_Y + CLOUD_HEIGHT - 30;
   var topScore = getMaxNumber(time);
+
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = '#000000';
+  ctx.fillText('Ура вы победили!', CLOUD_X + 20, CLOUD_Y + 20);
+  ctx.fillText('Список результатов:', CLOUD_X + 20, CLOUD_Y + 42);
 
   for (var i = 0; i < players.length; i++) {
     var heightGistPlayer = getHeightGist(time[i], topScore);
 
+    ctx.fillText(Math.floor(time[i]), GIST_X, GIST_Y - heightGistPlayer - 20);
+
     players[i] == 'Вы' ? ctx.fillStyle = 'red' : ctx.fillStyle = 'rgba(0, 0, 255, 0.' + i+7 + ')';
 
     printGist(ctx, GIST_X, GIST_Y - heightGistPlayer, heightGistPlayer);
-    printNames(ctx, players[i], GIST_X, GIST_POS);
+    printNames(ctx, players[i], GIST_X, NAME_POS);
     GIST_X += 100;
   }
 
@@ -62,12 +70,5 @@ var showWinners = function(ctx, players, time) {
 window.renderStatistics = function(ctx, players, time) {
   renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
-
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillStyle = '#000000';
-  ctx.fillText('Ура вы победили!', CLOUD_X + 20, CLOUD_Y + 20);
-  ctx.fillText('Список результатов:', CLOUD_X + 20, CLOUD_Y + 42);
-
-  showWinners(ctx, players, time);
+  printWinners(ctx, players, time);
 };
