96c7bfc6e6bd797e29812041a0175b18529b6cba
Igor Sudenko <sudenkoweb@gmail.com>
2019-03-08 08:59:41 +0200

    .setup: added hidePopup, openPopup;

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,10 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var setupOpen = document.querySelector('.setup-open');
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
@@ -28,6 +31,8 @@ var getRandomCoatColor = function(coatColors) {
 var getRandomEyesColor = function(eyesColors) {
     return eyesColors[getRandomNumber(eyesColors.length-1)];
 }
+
+var userNameInput = document.querySelector('.setup-user-name');
 
 // Mock
 var wizards = [
@@ -73,6 +78,72 @@ var printWizards = function() {
 }
 
 printWizards();
+
+var openPopup = function() {
+    userDialog.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+}
+
+var hidePopup = function() {
+    userDialog.classList.add('hidden');
+    document.removeEventListener('click', onPopupEscPress);
+}
+
+var onPopupEscPress = function(evt) {
+    if ( !(evt.target === userNameInput) ) {
+        if (evt.keyCode === ESC_KEYCODE) {
+            hidePopup();
+        }
+    }
+};
+
+var setupClose = document.querySelector('.setup-close');
+
+setupClose.addEventListener('click', function() {
+    hidePopup();
+});
+
+setupClose.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+        hidePopup();
+    }
+});
+
+setupOpen.addEventListener('click', function() {
+    openPopup();
+});
+
+setupOpen.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+        openPopup();
+    }
+});
+
+var setupFireball = document.querySelector('.setup-fireball-wrap');
+var changeFireballColor = function() {
+    setupFireball.style.backgroundColor = getRandomCoatColor(WIZARD_COAT_COLORS);
+}
+
+setupFireball.addEventListener('click', changeFireballColor);
+
+var changeCoatColor = function() {
+    wizardCoat.style.fill = getRandomCoatColor(WIZARD_COAT_COLORS);
+}
+
+var changeEyesColor = function() {
+    wizardEyes.style.fill = getRandomEyesColor(WIZARD_EYES_COLORS);
+}
+
+var wizardCoat = document.querySelector('.wizard-coat');
+wizardCoat.addEventListener('click', changeCoatColor);
+
+var wizardEyes = document.querySelector('.wizard-eyes');
+wizardEyes.addEventListener('click', changeEyesColor);
+
+
+
+
+
 
 
 
